7d7af0a2cb445fb3d182a88c2b1768f6715a6543
Iván García <ivann1922@gmail.com>
2017-11-11 21:21:42 -0600

    Creación de marcadores, arrastrarlos y obtener coordenadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+// Servicio
+import { MapasService } from './services/mapas.service';
+// Interface
+import { Marcador } from './interfaces/marcador';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,5 +16,23 @@ export class AppComponent {
   lat = 16.323050;
   lng = -96.594497;
   zoom = 16;
+
+  constructor ( public _ms: MapasService ) {
+
+  }
+
+  mapClicked ( evento ) {
+    const nMarcador: Marcador = {
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      titulo: 'Sin titulo',
+      draggable: true
+    };
+
+    console.log(evento);
+    this._ms.insertarMarcador(nMarcador);
+
+  }
+
 
 }
--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
 
+import { Marcador } from './../interfaces/marcador';
+
 @Injectable()
 export class MapasService {
 
-  constructor() { }
+  marcadores: Marcador[] = [];
+
+  constructor() {
+
+    const nuevoMarcador: Marcador = {
+      lat: 16.323050,
+      lng: -96.594497,
+      titulo: 'Mi casaaaa',
+      draggable: true
+    };
+
+    // Añadiendolo a nuestro arreglo de marcadores
+    this.marcadores.push( nuevoMarcador );
+  }
+
+  insertarMarcador ( marcador: Marcador) {
+    this.marcadores.push(marcador);
+  }
 
 }
